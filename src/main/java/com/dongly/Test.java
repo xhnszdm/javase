0877d895217fe66f0974132d4e55f6d3de4a78e1
兔八哥 <xhnszdm@aliyun.com>
2017-01-01 00:26:00 +0800

    create project

---
+++ src/main/java/com/dongly/Test.java
@@ -0,0 +1,12 @@
+package com.dongly;
+
+/**
+ * Created by bashen on 2017/1/1.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("1111111111");
+    }
+
+}
